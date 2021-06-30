83a57b590d5ff128e2f89864ee197ec3c92f7d51
Chai Woon Khai <chai1998@live.com>
2021-06-30 17:12:00 +0800

    reword some passages and minor refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { GiHumanPyramid } from 'react-icons/gi';
+import { IoHeartCircleOutline, IoNotificationsOutline, IoLogoGithub } from 'react-icons/io5';
 
 import Header from '../components/header';
 
 export default function Home() {
     return (
         <div>
-            <div
-                className="bg-cover bg-center absolute z-0 h-screen bg-no-repeat"
-                style={{
-                    backgroundImage: `url('http://preview.uxtheme.website/xpider-preview/xpider/img/hero-8.png')`
-                }}
-            />
+            <div className="bg-cover bg-center absolute z-0 h-screen bg-no-repeat" />
             <div
                 className="z-0 opacity-90 w-full h-full absolute"
                 style={{
@@ -22,11 +20,10 @@ export default function Home() {
                 <div
                     className="bg-bottom bg-no-repeat bg-cover h-screen"
                     style={{
-                        backgroundImage:
-                            "url('https://galaxy.up.video/media/backgrounds/bg-curve.svg')"
+                        backgroundImage: "url('bg-curve.svg')"
                     }}>
                     <Header />
-                    <section className="w-full ">
+                    <section className="w-full">
                         <div className="container mx-auto flex md:flex-row sm:items-center xl:pb-16 xl:px-0 px-4">
                             <div className="w-1/2 text-white md:flex md:flex-col md:space-y-6">
                                 <h1 className="sm:text-5xl text-xl font-weight-bolder sm:mb-4 text-black md:text-white">
@@ -38,20 +35,16 @@ export default function Home() {
                                 <p className="text-xs sm:text-base text-black md:text-white">
                                     BILA AKU BOLEH KENA CUCUK!?
                                 </p>
-                                <div className="xl:mt-4 mt-8 flex flex">
+                                <div className="xl:mt-4 mt-8 flex">
                                     <Link href="/subscribe">
-                                        <button className="w-4/5 md:w-full py-8 md:px-8 md:py-4 rounded-3xl bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 ">
+                                        <button className="w-4/5 md:w-full py-8 md:px-8 md:py-4 rounded-3xl bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2">
                                             I nak SMS when appointment is ready{' 💬 '}
                                         </button>
                                     </Link>
                                 </div>
                             </div>
                             <div className="w-1/2 xl:pt-8 xl:items-center flex justify-end">
-                                <img
-                                    className="xl:w-2/5"
-                                    alt="Screenshot of Mobile phone of this application"
-                                    src="phone.png"
-                                />
+                                <Image src="/phone.png" width={256} height={520} />
                             </div>
                         </div>
                     </section>
@@ -68,94 +61,82 @@ export default function Home() {
                             <div className="sm:w-1/3 mb-8 sm:mb-0 xl:pr-0 pr-2">
                                 <div className="flex flex-wrap mb-4 mt-0">
                                     <div className="w-1/6 items-center mx-auto">
-                                        <img
-                                            src="https://img.icons8.com/material-outlined/48/000000/appointment-reminders--v2.png"
-                                            alt="Notification Icon"
-                                        />
+                                        <IoNotificationsOutline size={48} />
                                     </div>
                                     <div className="w-5/6">
-                                        <h4 className="text-black items-center xl:text-2xl capitalize">
-                                            Better Notification
+                                        <h4 className="text-black items-center xl:text-2xl">
+                                            Guaranteed notification
                                         </h4>
                                     </div>
                                 </div>
                                 <div className="leading-relaxed pr-4">
                                     <p>
                                         <b className="font-normal text-blue-600">
-                                            No more Checking MySejahtera.
-                                        </b>{' '}
+                                            Don't worry, we are not as forgetful
+                                        </b>
                                     </p>
                                     <p>
-                                        Now, this software will send you an SMS when your
-                                        appointment is ready.
+                                        We promise that you will receive an SMS from us if your
+                                        vaccination appointment is ready or even changed.
                                     </p>
                                 </div>
                             </div>
                             <div className="sm:w-1/3 mb-8 sm:mb-0 xl:pr-0 pr-2">
                                 <div className="flex flex-wrap mb-4">
                                     <div className="w-1/6 items-center mx-auto">
-                                        <img
-                                            src="https://img.icons8.com/material-rounded/48/000000/family.png"
-                                            alt="Family Icon"
-                                        />
-                                    </div>
-                                    <div className="w-4/5">
-                                        <h4 className="text-black items-center xl:text-2xl capitalize">
-                                            Inform your family
-                                        </h4>
-                                    </div>
-                                </div>
-                                <div className="leading-relaxed pr-4">
-                                    <p>
-                                        <b className="font-normal text-blue-600 ">
-                                            Notify Your Love one that your appointment is ready
-                                        </b>{' '}
-                                    </p>
-                                    <p>
-                                        Getting a jab is not a single person job. Let your family
-                                        knowing that your vaccine appointment is prepared to make
-                                        you arrange your schedule easily.
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="sm:w-1/3 mb-8 sm:mb-0 xl:pr-0 pr-2">
-                                <div className="flex flex-wrap mb-4">
-                                    <div className="w-1/6 items-center mx-auto">
-                                        <img
-                                            src="https://img.icons8.com/material-outlined/48/000000/money--v2.png"
-                                            alt="Money Icon"
-                                        />
+                                        <IoHeartCircleOutline size={48} />
                                     </div>
                                     <div className="w-4/5">
                                         <h4 className="text-black items-center xl:text-2xl">
-                                            Not A Multi Billion Project
+                                            Care for your loved one
                                         </h4>
                                     </div>
                                 </div>
                                 <div className="leading-relaxed pr-4">
                                     <p>
                                         <b className="font-normal text-blue-600">
-                                            No more wasting rakyat's money
+                                            Get up to speed with your family member's vaccine
+                                            appointment updates
                                         </b>
                                     </p>
-                                    It is made open-source, the developer made it with the hope that
-                                    our country will get better in days.
+                                    <p>
+                                        Keep an eye out for your grandparents, parents and even
+                                        childrens when their appointed is set up.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="sm:w-1/3 mb-8 sm:mb-0 xl:pr-0 pr-2">
+                                <div className="flex flex-wrap mb-4">
+                                    <div className="w-1/6 items-center mx-auto">
+                                        <GiHumanPyramid size={48} />
+                                    </div>
+                                    <div className="w-4/5">
+                                        <h4 className="text-black items-center xl:text-2xl">
+                                            Made by Malaysians for everyone
+                                        </h4>
+                                    </div>
+                                </div>
+                                <div className="leading-relaxed pr-4">
+                                    <p>
+                                        <b className="font-normal text-blue-600">
+                                            We do what we can, we are in this together
+                                        </b>
+                                    </p>
+                                    This is an open-source initiative by Malaysians. Your
+                                    contributions and donations are very welcomed.
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <footer className=" text-center py-8 border-t border-gray-200 text-xs text-gray-700 w-full">
-                        <a href="https://icons8.com/icon/AszNi9Jvemeu/notification">
-                            Notification icon by Icons8
-                        </a>
-                        <br />
-                        <a href="https://icons8.com/icon/t2abv2VHiC8x/money">
-                            Money icon by Icons8
-                        </a>
-                        <br />
-                        <a href="https://icons8.com/icon/86222/family">Family icon by Icons8</a>
+                    <footer className="text-center py-8 border-t border-gray-200 text-xs text-gray-700 w-full">
+                        An initiative by Malaysians for everyone
+                        <div className="mt-1">
+                            <a href="https://github.com/nubpro/nakvaksin">
+                                <IoLogoGithub size={20} className="inline-block" />
+                            </a>
+                        </div>
                     </footer>
                 </div>
             </div>
