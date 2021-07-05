1568eb855ba9f8e2edfcc5c5718bbc8144a84b3d
chengkangzai <pycck@hotmail.com>
2021-07-05 17:16:24 +0800

    remove unnecessary header

--- pages/api/forgetPassword.ts
+++ pages/api/forgetPassword.ts
@@ -6,15 +6,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         await axios({
             method: 'post',
             url: 'https://mysejahtera.malaysia.gov.my/register/forgotPassword',
-            headers: {
-                Connection: 'keep-alive',
-                'Accept-Language': 'en-MY;q=1, zh-Hans-MY;q=0.9',
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                'Content-Length': '33',
-                'User-Agent': 'MySejahtera/1.0.36 (iPhone; iOS 14.4.2; Scale/2.00)',
-                Host: 'mysejahtera.malaysia.gov.my'
-            },
             data: {
                 emailOrUserName: req.body.emailOrUserName
             }
