d1d2bb03be1eeaf56bbe5c6999ec63e68b565592
chengkangzai <pycck@hotmail.com>
2021-07-06 18:23:01 +0800

    really make sure the data is correct

--- pages/api/resetpassword.ts
+++ pages/api/resetpassword.ts
@@ -1,14 +1,28 @@
 import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import formatPhoneNumber from '../../utlis/formatPhoneNumber';
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (
+        !req.body.username.includes('@') &&
+        !req.body.username.includes('+') &&
+        !req.body.username.includes('-') &&
+        !req.body.username.match('^[0-9]+$')
+    ) {
+        return res.status(400).json({
+            message: 'Invalid Input'
+        });
+    }
     try {
         await axios({
             method: 'post',
             url: 'https://mysejahtera.malaysia.gov.my/register/forgotPassword',
             data: {
-                emailOrUserName: req.body.username
-            }
+                emailOrUserName: formatPhoneNumber(req.body.username)
+            },
+            timeout: 5000,
+            timeoutErrorMessage: 'TIMEOUT'
         });
 
         res.status(200).json({
