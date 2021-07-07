58ac453c3fc5068b2dadf1534a8f60959baaf1b0
chengkangzai <pycck@hotmail.com>
2021-07-07 16:57:33 +0800

    reformat the code

--- utils/sanitizePhoneNumber.ts
+++ utils/sanitizePhoneNumber.ts
@@ -1,25 +1,23 @@
 function sanitizePhoneNumber(usernameValue: string): string {
-    const isEmail = usernameValue.includes('@');
-    let newUsernameValue = usernameValue;
-
-    if (isEmail) {
+    /**
+     * Return because it is Email
+     */
+    if (usernameValue.includes('@')) {
         return usernameValue;
-    } else {
-        // Prefix it with 6 if user enter "012..." without country code (we just assume it's MY number)
-        if (newUsernameValue[0] === '0') {
-            newUsernameValue = '6' + newUsernameValue;
-        }
-
-        // TODO: maybe only check the first 3 characters only for sanity
-        if (newUsernameValue.includes('+60')) {
-            newUsernameValue = newUsernameValue.replace('+60', '60');
-        }
-
-        newUsernameValue.replace(/\-/g, '');
-        newUsernameValue.replace(/\s/g, ''); // replace space
-
-        return newUsernameValue;
     }
+
+    let newUsernameValue = usernameValue;
+    /**
+     * Prefix it with 6 if user enter "012..." without country code (we just assume it's MY number)
+     */
+    if (newUsernameValue[0] === '0') {
+        newUsernameValue = '6' + newUsernameValue;
+    }
+
+    newUsernameValue = newUsernameValue.replace('+60', '60');
+    newUsernameValue = newUsernameValue.replaceAll('-', '');
+    newUsernameValue = newUsernameValue.replaceAll(' ', '');
+    return newUsernameValue;
 }
 
 export default sanitizePhoneNumber;
