82f797c8e65dd19a2d461e09782e3b05ce17e84e
chengkangzai <pycck@hotmail.com>
2021-07-07 20:53:35 +0800

    use regex to test email

--- utils/username.ts
+++ utils/username.ts
@@ -1,5 +1,8 @@
 function isEmail(username: string): boolean {
-    return username.includes('@');
+    const regex = new RegExp(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    );
+    return regex.test(username);
 }
 
 function isPhoneNumber(username: string): boolean {
