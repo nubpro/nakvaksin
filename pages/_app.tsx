1726c88b732be4aaaff7d244b78ac229fb904308
chengkangzai <pycck@hotmail.com>
2021-07-15 19:11:07 +0800

    use React Query Dev tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,22 @@ import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { CookiesProvider } from 'react-cookie';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import SEO from '../components/SEO';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const queryClient = new QueryClient();
     return (
         <CookiesProvider>
-            <Head>
-                <SEO />
-            </Head>
-            <Component {...pageProps} />
+            <QueryClientProvider client={queryClient}>
+                <Head>
+                    <SEO />
+                </Head>
+                <Component {...pageProps} />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
         </CookiesProvider>
     );
 }
