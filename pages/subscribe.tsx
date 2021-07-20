4c7c5cffaf708a1d7b018ca2b4839b46246c0b47
Chai Woon Khai <chai1998@live.com>
2021-07-21 01:14:50 +0800

    ui refactor (wip)

--- pages/subscribe.tsx
+++ pages/subscribe.tsx
@@ -39,87 +39,91 @@ export default function Subscribe() {
         });
     });
 
+    const textFieldStyle =
+        'border border-black py-5 rounded-full text-center w-full placeholder-gray-700 font-light outline-none';
+    const groupHeadingStyle = 'text-xl font-semibold mb-3';
+
     return (
-        <div className="container mx-auto px-4 pt-5">
-            <Header />
+        <div className="container mx-auto flex flex-col h-screen">
+            <div className="text-center italic font-light py-7">
+                All fields are optional
+                <br />
+                You may leave them blank
+            </div>
 
-            <form className="text-center space-y-6" onSubmit={onSubmit}>
-                {isSubmitting && (
-                    <div className="absolute bg-black bg-opacity-60 w-full h-full z-10 left-0 top-0 flex flex-col justify-center items-center">
-                        <svg
-                            className="animate-spin h-8 w-8 text-white"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24">
-                            <circle
-                                className="opacity-25"
-                                cx="12"
-                                cy="12"
-                                r="10"
-                                stroke="currentColor"
-                                strokeWidth="4"
-                            />
-                            <path
-                                className="opacity-75"
-                                fill="currentColor"
-                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                            />
-                        </svg>
+            <form className="text-center flex-1 flex flex-col" onSubmit={onSubmit}>
+                <div className="flex-1">
+                    <div>
+                        <div className={groupHeadingStyle}>
+                            Subscribe <span className="underline">me</span> to my vaccination status
+                        </div>
+
+                        <div className="flex flex-col space-y-4">
+                            <div>
+                                <input
+                                    id="phone"
+                                    type="text"
+                                    className={textFieldStyle}
+                                    placeholder="Please enter your phone number"
+                                    {...register('phone', { required: false })}
+                                />
+                                {user?.phoneNumber && (
+                                    <FieldUnderCaption caption={user.phoneNumber} />
+                                )}
+                            </div>
+
+                            <div>
+                                <input
+                                    id="email"
+                                    type="email"
+                                    className={textFieldStyle}
+                                    placeholder="Please enter your email"
+                                    {...register('email', { required: false })}
+                                />
+                                {user?.email && <FieldUnderCaption caption={user.email} />}
+                            </div>
+                        </div>
                     </div>
-                )}
-                <h4 className="text-xl font-semibold w-3/4 mx-auto ">
-                    Subscribe me to my vaccination status
-                </h4>
-                <input
-                    id="phone"
-                    type="text"
-                    className="border border-8 py-4 w-full border-blue-500 rounded-full text-center"
-                    placeholder="Please enter your phone number"
-                    {...register('phone', { required: false })}
-                />
-                {isPhoneNumber(user?.username) && (
-                    <span>
-                        Use <button className="text-blue-500 underline">{user?.username}</button>
-                    </span>
-                )}
-                <input
-                    id="email"
-                    type="email"
-                    className="border border-8 py-4 w-full border-blue-500 rounded-full text-center mt-2"
-                    placeholder="Please enter your email"
-                    {...register('email', { required: false })}
-                />
-                {isEmail(user?.username) && (
-                    <span>
-                        Use <button className="text-blue-500 underline">{user?.username}</button>
-                    </span>
-                )}
 
-                <h4 className="text-xl font-semibold w-3/4 mx-auto pt-12">
-                    Update my family about my vaccination status (optional)
-                </h4>
-                <div className="space-y-4">
-                    <input
-                        id="fam_phone"
-                        type="text"
-                        className="border border-8 py-4 w-full border-blue-500 rounded-full text-center"
-                        placeholder="Please enter their phone number"
-                        {...register('fam_phone', { required: false })}
-                    />
-                    <input
-                        id="fam_email"
-                        type="email"
-                        className="border border-8 py-4 w-full border-blue-500 rounded-full text-center"
-                        placeholder="Please enter their email"
-                        {...register('fam_email', { required: false })}
-                    />
+                    <div className="mt-10">
+                        <div className={groupHeadingStyle}>
+                            Subscribe <span className="underline">my family</span> to my vaccination
+                            status
+                        </div>
+
+                        <div className="flex flex-col space-y-4">
+                            <input
+                                id="fam_phone"
+                                type="text"
+                                className={textFieldStyle}
+                                placeholder="Please enter their phone number"
+                                {...register('fam_phone', { required: false })}
+                            />
+                            <input
+                                id="fam_email"
+                                type="email"
+                                className={textFieldStyle}
+                                placeholder="Please enter their email"
+                                {...register('fam_email', { required: false })}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="">
                     <button
                         type="submit"
-                        className="btn bg-green-500 text-white border-4 border-green-500 rounded-2xl w-full py-2 mt-2 font-bold">
-                        Subscribe !
+                        className="btn bg-green-500 text-white border-4 border-green-500 rounded-2xl w-full py-2">
+                        Save
                     </button>
                 </div>
             </form>
         </div>
     );
 }
+
+const FieldUnderCaption = ({ caption }: { caption: string }) => (
+    <div>
+        Use <button className="text-blue-500 underline pt-1.5 pb-1 font-medium">{caption}</button>
+    </div>
+);
