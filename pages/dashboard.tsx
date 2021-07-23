ff3453ee9b98a98d15aa2da43d7c8a18f7a15525
chengkangzai <pycck@hotmail.com>
2021-07-23 22:27:02 +0800

    Show Both VaxStatus and Subscribe Status in dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,17 @@
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import Header from '../components/header';
-import VaxStatus from '../components/vaxStatus';
-import { useUser } from '../hooks/useUser';
+import SubscribeStatus from '../components/SubscribeStatus';
+import VaxStatus from '../components/VaxStatus';
 
 export default function dashboard() {
-    const { user } = useUser();
-    const router = useRouter();
     return (
         <div className="container mx-auto px-4 pt-5">
             <Header />
-            <VaxStatus />
+            <div className="space-y-3">
+                <VaxStatus />
+                <SubscribeStatus />
+            </div>
         </div>
     );
 }
