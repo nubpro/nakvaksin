c43d0cd30f2e436a2195a0ee7c6c152d88b80f7b
Chai Woon Khai <chai1998@live.com>
2021-07-31 02:08:49 +0800

    add getServersideProps to dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,26 @@
+import { GetServerSideProps } from 'next';
 import React from 'react';
+import { QueryClient } from 'react-query';
+import { dehydrate } from 'react-query/hydration';
 
 import Header from '../components/header';
 import SubscribeStatus from '../components/SubscribeStatus';
 import VaxStatus from '../components/VaxStatus';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const queryClient = new QueryClient();
+
+    const { userProfile } = context.req.cookies;
+    if (userProfile) {
+        queryClient.setQueryData('user', JSON.parse(userProfile));
+    }
+
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient)
+        }
+    };
+};
 
 export default function dashboard() {
     return (
