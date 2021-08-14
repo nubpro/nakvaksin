06773a0007a3eda28baa4dc2ffa7bc9dcc72feda
chengkangzai <pycck@hotmail.com>
2021-08-14 13:34:44 +0800

    improve the logic and User Interface

--- pages/r/[key].tsx
+++ pages/r/[key].tsx
@@ -6,7 +6,8 @@ import { reSub, unSub } from '../../services/SubUnSub';
 
 export default function key() {
     const [isUnSubing, setIsUnSubing] = useState(true);
-    const [isReSubing, setIsReSubing] = useState(false);
+    const [isReSubing, setIsReSubing] = useState(true);
+    const [isDoneReSubing, setIsDoneReSubing] = useState(false);
 
     const router = useRouter();
     const { key } = router.query;
@@ -18,7 +19,9 @@ export default function key() {
     }
 
     function undo() {
+        setIsReSubing(false);
         reSub(key).then(() => {
+            setIsDoneReSubing(true);
             setIsReSubing(true);
         });
     }
@@ -28,12 +31,12 @@ export default function key() {
             <Header />
 
             <div className="mx-auto text-center space-y-2">
-                {(!isUnSubing || isReSubing) && (
+                {(isUnSubing || !isReSubing) && (
                     <div>
                         <button className="bg-blue-500 rounded rounded-full p-6 cursor-default animate-bounce my-2" />
                     </div>
                 )}
-                {isUnSubing && (
+                {!isUnSubing && !isDoneReSubing && (
                     <>
                         <h1>You have been unsubscribed to the service.</h1>
                         <h2 className="mt-4">Whoops! I didnt meant to do that?</h2>
@@ -42,15 +45,21 @@ export default function key() {
                             className="text-white bg-blue-500 px-4 py-2 rounded-xl">
                             Undo!
                         </button>
+                        <hr />
+                        <h1 className="pt-8 text-sm">
+                            Its OK, we understand... <br />
+                            But we not gonna pretend we are not sad about it
+                            <span className="text-2xl">{' 😢 '}</span>
+                        </h1>
                     </>
                 )}
 
-                <hr />
-                <h1 className="pt-8 text-sm">
-                    Its OK, we understand... <br />
-                    But we not gonna pretend we are not sad about it
-                    <span className="text-2xl">{' 😢 '}</span>
-                </h1>
+                {isDoneReSubing && (
+                    <>
+                        <h1>Hooray! Your subscription to the service is restore.</h1>
+                        <span className="text-4xl mt-2"> {'🎉🎉'}</span>
+                    </>
+                )}
             </div>
         </div>
     );
