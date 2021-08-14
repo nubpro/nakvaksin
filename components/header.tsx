7d7c464b4045a737f2794ec9f7f37dbed8aa6649
Ching Cheng Kang <pycck@hotmail.com>
2021-08-15 00:03:14 +0800

    remove /faq as public route

--- components/header.tsx
+++ components/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
 
     useEffect(() => {
         // TODO: Move this outside of header, probably in _app.tsx (need to create an AuthProvider I think)
-        const publicRoutes = ['/', '/login', '/resetpassword', '/faq', '/r'];
+        const publicRoutes = ['/', '/login', '/resetpassword', '/r'];
         const isPublicRoute = publicRoutes.includes(router.pathname);
         const isUnSubRoute = publicRoutes.filter((r) => r.match(/[/r]/g));
         const isAuthenticated = !!user;
