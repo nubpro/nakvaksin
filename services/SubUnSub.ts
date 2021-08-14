199f7cac40a4fbf21418cc6ac0db4c013a0d6828
chengkangzai <pycck@hotmail.com>
2021-08-14 13:22:04 +0800

    fix bug: method change to GET

--- services/SubUnSub.ts
+++ services/SubUnSub.ts
@@ -3,14 +3,14 @@ import axios from 'axios';
 async function unSub(key: string | string[]) {
     return axios({
         url: 'https://nakvaksin-backend-6cmnfscq7a-as.a.run.app/nakvaksin/api/v1/unsub/' + key,
-        method: 'POST'
+        method: 'GET'
     });
 }
 
 async function reSub(key: string | string[] | undefined) {
     return axios({
         url: 'https://nakvaksin-backend-6cmnfscq7a-as.a.run.app/nakvaksin/api/v1/resub/' + key,
-        method: 'POST'
+        method: 'GET'
     });
 }
 
