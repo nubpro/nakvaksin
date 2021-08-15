3782e8268c863bd909ff252acbe5e270d40d61d4
Ching Cheng Kang <pycck@hotmail.com>
2021-08-15 17:57:29 +0800

    add Call to action in dashboard (#41)

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,7 @@
 import { GetServerSideProps } from 'next';
+import Link from 'next/link';
 import React from 'react';
+import { AiOutlineBell } from 'react-icons/ai';
 import { QueryClient } from 'react-query';
 import { dehydrate } from 'react-query/hydration';
 
@@ -22,11 +24,30 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     };
 };
 
+const CTLDashboard = () => {
+    return (
+        <Link href="/subscribe">
+            <button className="mx-auto w-full bg-primary text-white flex py-4 px-2 rounded-2xl">
+                <AiOutlineBell className="flex-none text-4xl self-center mr-2" />
+                <div className="flex-grow">
+                    <h3 className="text-left font-bold text-sm">
+                        Subscribe to my appointment updates
+                    </h3>
+                    <p className="text-left text-white opacity-80 text-xs">
+                        We will keep you (and your family) updated when your vaccination appointment
+                        changes
+                    </p>
+                </div>
+            </button>
+        </Link>
+    );
+};
 export default function dashboard() {
     return (
         <div className="container mx-auto px-4 pt-5">
             <Header />
             <div className="space-y-3">
+                <CTLDashboard />
                 <VaxStatus />
                 <SubscribeStatus />
             </div>
