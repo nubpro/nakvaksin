9aa4d5f509581939b7d454117a055a1418f39241
Chai Woon Khai <chai1998@live.com>
2021-08-15 20:57:22 +0800

    add credits

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,11 @@ import { IoLogoGithub } from 'react-icons/io5';
 export default function Footer() {
     return (
         <footer className="mt-8 text-center py-6 border-t border-gray-200 text-xs text-gray-700 w-full">
-            An initiative by Malaysians for everyone
+            <div>NakVaksin | An initiative by Malaysians for everyone</div>
+            <div>Made by Ching Cheng Kang, WC Chan, Chai Woon Khai</div>
+            <div className="mt-2">
+                <a href="mailto:nakvaksin@gmail.com">Need help? Email us</a>
+            </div>
             <div className="mt-1">
                 <a href="https://github.com/nubpro/nakvaksin">
                     <IoLogoGithub size={20} className="inline-block" />
