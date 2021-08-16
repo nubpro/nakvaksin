e27f3f9704573af534cd7033fabc50a1a9b7b074
chengkangzai <pycck@hotmail.com>
2021-08-16 20:50:54 +0800

    add link to chengkangzai website and add rel to the link tag

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,9 +4,17 @@ export default function Footer() {
     return (
         <footer className="mt-8 text-center py-6 border-t border-gray-200 text-xs text-gray-700 w-full">
             <div>NakVaksin | An initiative by Malaysians for everyone</div>
-            <div>Made by Ching Cheng Kang, Julius Chan, Chai Woon Khai</div>
+            <div>
+                Made by{' '}
+                <a href="https://www.chengkangzai.com/" rel="external noreferrer" target="_blank">
+                    Ching Cheng Kang
+                </a>
+                , Julius Chan, Chai Woon Khai
+            </div>
             <div className="mt-2">
-                <a href="mailto:nakvaksin@gmail.com">Need help? Email us</a>
+                <a href="mailto:nakvaksin@gmail.com" rel="author">
+                    Need help? Email us
+                </a>
             </div>
             <div className="mt-1">
                 <a href="https://github.com/nubpro/nakvaksin">
