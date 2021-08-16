b84fbd968a0ceb08509145752bedeadb25d5bef9
Ching Cheng Kang <pycck@hotmail.com>
2021-08-16 22:21:39 +0800

    add google analytic (#46)

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -36,6 +36,14 @@ export default function SEO() {
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <link rel="manifest" href="/site.webmanifest" />
+            <script async src='https://www.googletagmanager.com/gtag/js?id=G-9BJ58SKGY6'/>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-9BJ58SKGY6');
+            </script>
         </>
     );
 }
