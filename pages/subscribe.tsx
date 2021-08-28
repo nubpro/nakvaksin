7d13e2eab61adef943bd340d3ddf7c4bfa63a799
Chai <chai1998@live.com>
2021-08-28 12:48:49 +0800

    feat: Unsubscribe from nakvaksin (#55)
    * feat: add notice and privacy page
    
    * fix: sanitize user phone number
    
    * feat: unsubscribe from nakvaksin completely
    
    * fix onsubmit

--- pages/subscribe.tsx
+++ pages/subscribe.tsx
@@ -349,7 +349,21 @@ export default function Subscribe() {
 
                         <button
                             className="my-2 py-2 text-sm flex items-center text-red-500 hover:text-red-700"
-                            type="button">
+                            type="button"
+                            onClick={async () => {
+                                const yes = confirm(
+                                    'Are you sure you want to unsubscribe from NakVaksin?\nYou and your family will stop receiving notification from us'
+                                );
+                                if (yes) {
+                                    setValue('userPhoneNumber', '');
+                                    setValue('userEmail', '');
+                                    setValue('familyPhoneNumber', '');
+                                    setValue('familyEmail', '');
+
+                                    await onSubmit();
+                                    router.replace('dashboard');
+                                }
+                            }}>
                             <BiTrash size={22} />
                             <div className="ml-1 font-medium">Unsubscribe from NakVaksin</div>
                         </button>
